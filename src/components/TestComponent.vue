9be8abcedc9b9a9ad3a5328b56227205db2f9d47
FabricioCCapone <fabricapone12@gmail.com>
2025-05-28 11:43:34 +0200

    Second commit: testing vuetify

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+// import { ref } from "vue";
+
+// const backlog = ref(["task 1", "task 2", "task 3"]);
+// const inProgress = ref([]);
+// const testing = ref([]);
+// const done = ref([]);
+
+// const changeStatus = (task, from, to) => {
+//   if (Array.isArray(from) && Array.isArray(to) && from.includes(task)) {
+//     from.value = from.value.filter(item => item !== task);
+//     to.value = [...to.value, task];
+//   } else {
+//     console.error("Invalid parameters: 'from' and 'to' must be arrays.");
+//   } 
+// };
+
+</script>
+<template>
+<!-- <button @click="changeStatus('task 1', backlog, inProgress)">Change status</button>
+<ul>
+  <li v-for="task in backlog.value" :key="task">
+    {{ task }}
+  </li>
+</ul>
+<ul>
+  <li v-for="task in inProgress.value" :key="task">
+    {{ task }}
+  </li>
+</ul> -->
+</template>
+
+
